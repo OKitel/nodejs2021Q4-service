a31dbcf531c6635db437eeb3742f04951681a628
OKitel <kitelolga@gmail.com>
2021-12-09 01:02:38 +0100

    refactor: change export const in config

--- src/common/config.ts
+++ src/common/config.ts
@@ -5,7 +5,11 @@ dotenv.config({
   path: path.join(__dirname, '../../.env'),
 });
 
-export const { PORT, NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY } =
-  process.env;
+export const {
+  PORT = 4000,
+  NODE_ENV,
+  MONGO_CONNECTION_STRING,
+  JWT_SECRET_KEY,
+} = process.env;
 
 export const AUTH_MODE = process.env.AUTH_MODE === 'true';
