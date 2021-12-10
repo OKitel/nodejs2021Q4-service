3f7b8ac5770a670a873a36b87ba0000fa499e44f
OKitel <kitelolga@gmail.com>
2021-12-10 16:35:43 +0100

    docs: add tsdoc for board service

--- src/resources/boards/board.service.ts
+++ src/resources/boards/board.service.ts
@@ -2,11 +2,21 @@ import { boardsRepo } from './board.memory.repository';
 import { tasksRepo } from '../tasks/task.memory.repository';
 import { Board } from './board.model';
 
+/**
+ * Returns all boards
+ * @returns all boards
+ */
 const getAll = async () => {
   const boards = await boardsRepo.getAll();
   return boards;
 };
 
+/**
+ * Returns the single board by ID
+ * @param id board ID
+ * @throws an error if a board with a passed ID hasn't been found
+ * @returns board by ID
+ */
 const getOne = async (id: string) => {
   const board = await boardsRepo.getOne(id);
   if (!board) {
@@ -15,15 +25,29 @@ const getOne = async (id: string) => {
   return board;
 };
 
+/**
+ * Delete board by ID
+ * @param id board ID
+ */
 const deleteById = async (id: string) => {
   await tasksRepo.deleteTasksByBoardId(id);
   await boardsRepo.deleteById(id);
 };
 
+/**
+ * Save board
+ * @param board see type {@link Board}
+ */
 const save = async (board: Board) => {
   await boardsRepo.save(board);
 };
 
+/**
+ * Update board by ID
+ * @param board see type {@link Board}
+ * @throws an error if a board with a passed ID hasn't been found
+ * @returns updated board
+ */
 const update = async (board: Board) => {
   const oldBoard = await boardsRepo.getOne(board.id);
   if (!oldBoard) {
