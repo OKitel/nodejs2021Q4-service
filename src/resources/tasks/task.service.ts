e90bb7a008a0b9ccdaa363c5ce64bad3c6d1368e
OKitel <kitelolga@gmail.com>
2021-12-10 17:03:23 +0100

    docs: add tsdoc for task service

--- src/resources/tasks/task.service.ts
+++ src/resources/tasks/task.service.ts
@@ -2,6 +2,12 @@ import { tasksRepo } from './task.memory.repository';
 import { boardsRepo } from '../boards/board.memory.repository';
 import { Task } from './task.model';
 
+/**
+ * Returns all tasks by board ID
+ * @param id board ID
+ * @throws an error if a board with a passed ID hasn't been found
+ * @returns an array of tasks
+ */
 const getAllTasksByBoardId = async (id: string) => {
   const board = await boardsRepo.getOne(id);
   if (!board) {
@@ -11,6 +17,13 @@ const getAllTasksByBoardId = async (id: string) => {
   return tasks;
 };
 
+/**
+ * Returns single task by board ID and task ID
+ * @param boardId board ID
+ * @param taskId task ID
+ * @throws an error if a task with a passed ID hasn't been found
+ * @returns task
+ */
 const getOne = async (boardId: string, taskId: string) => {
   const task = await tasksRepo.getOne(boardId, taskId);
   if (!task) {
@@ -19,14 +32,28 @@ const getOne = async (boardId: string, taskId: string) => {
   return task;
 };
 
+/**
+ * Delete task by ID
+ * @param id task ID
+ */
 const deleteById = async (id: string) => {
   await tasksRepo.deleteById(id);
 };
 
+/**
+ * Save task
+ * @param task see type {@link Task}
+ */
 const save = async (task: Task) => {
   await tasksRepo.save(task);
 };
 
+/**
+ * Update task
+ * @param task see type {@link Task}
+ * @throws an error if a task with a passed ID hasn't been found
+ * @returns updated task
+ */
 const update = async (task: Task) => {
   const oldTask = await tasksRepo.getOne(task.boardId, task.id);
   if (!oldTask) {
