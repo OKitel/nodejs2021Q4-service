de19998c4cbb7f7949ce8b459916b36b6e91715c
OKitel <kitelolga@gmail.com>
2022-01-23 19:25:21 +0100

    refactor: extract fastifyTokenVerifier

--- src/server.ts
+++ src/server.ts
@@ -9,9 +9,9 @@ import { userRoutes } from './routes/users.router';
 import { Logger, pinoLogger } from './logger';
 import { globalErrorHandler } from './errors';
 import { loginRoute } from './routes/login.router';
-import { verifyToken } from './auth/tokenUtils';
+import { fastifyTokenVerifier } from './auth/fastifyTokenVerifier';
 
-const server = fastify({
+export const server = fastify({
   logger: pinoLogger,
 });
 
@@ -37,16 +37,7 @@ server.register(userRoutes);
 server.register(taskRoutes);
 server.register(loginRoute);
 
-server.addHook('preHandler', async (req) => {
-  if (req.url.startsWith('/boards') || req.url.startsWith('/users')) {
-    const userData = await verifyToken(req.headers.authorization);
-    Logger.info(
-      req,
-      userData,
-      `User with login ${userData.login} successfully authorized`
-    );
-  }
-});
+fastifyTokenVerifier(server);
 
 /**
  * Start server on port from .env
