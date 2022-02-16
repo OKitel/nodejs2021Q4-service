37bb85bb6d8866aec668d583e7a8a174624b4d7e
OKitel <kitelolga@gmail.com>
2022-02-16 22:12:34 +0100

    feat: add swagger decorations for login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,33 @@
 import { Controller, Request, Post, UseGuards, HttpCode } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { LoginRequestDto } from './auth/dto/login-request.dto';
+import { LoginResponseDto } from './auth/dto/login-response.dto';
 
+@ApiTags('login')
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({
+    summary: 'Authenticate user',
+    description: 'Returns user jwt',
+  })
+  @ApiBody({ type: LoginRequestDto })
+  @ApiResponse({
+    status: 200,
+    description: 'jwt',
+    type: LoginResponseDto,
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
   @UseGuards(LocalAuthGuard)
   @Post('login')
   @HttpCode(200)
