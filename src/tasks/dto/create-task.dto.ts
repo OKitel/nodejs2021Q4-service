ecdae84f282fd024161f81094a7795975ff8129d
OKitel <kitelolga@gmail.com>
2022-02-16 21:47:41 +0100

    feat: add swagger decorators for tasks

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,14 +1,24 @@
-import { IsString, IsNumber } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsString, IsNumber, IsOptional } from 'class-validator';
 
 export class CreateTaskDto {
+  @ApiProperty()
   @IsString()
   title: string;
+
+  @ApiProperty()
   @IsNumber()
   order: number;
+
+  @ApiProperty()
   @IsString()
   description: string;
 
+  @ApiPropertyOptional()
+  @IsOptional()
   userId: string | null;
 
+  @ApiPropertyOptional()
+  @IsOptional()
   columnId: string | null;
 }
--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,17 +1,31 @@
-import { IsString, IsNumber } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsString, IsNumber, IsOptional } from 'class-validator';
 export class TaskDto {
+  @ApiProperty()
   @IsString()
   id: string;
+
+  @ApiProperty()
   @IsString()
   title: string;
+
+  @ApiProperty()
   @IsNumber()
   order: number;
+
+  @ApiProperty()
   @IsString()
   description: string;
 
+  @ApiPropertyOptional()
+  @IsOptional()
   userId: string | null;
+
+  @ApiProperty()
   @IsString()
   boardId: string;
 
+  @ApiPropertyOptional()
+  @IsOptional()
   columnId: string | null;
 }
